359e01958669d9d55e341d4d19e3470495f9174f
Matt Coston <mattcoston7@gmail.com>
2020-10-27 10:10:10 -0500

    Wrapped up some logic on update and delete

--- controllers/users.js
+++ controllers/users.js
@@ -2,43 +2,38 @@ const users = require("../data/index")
 
 
 const getUsers = (req, res, next) => {
-    //if(!users){
-      //  throw new Error("Not supplied Users!")
-    //}
     res.send(users)
 }
 
 const getUsersById = (req, res, next ) => {
     let id = parseInt(req.params.id)
     let user = users.find((user) => user.id === id)
-
-    //if(!users.params.id){
-      //  throw new Error("Need Users Id!")
-    //}
     res.send(user)
 }
 
 const create = (req, res, next ) => {
     let messagePayload = req.body
-    messagePayload.id = users.length + 1
+    messagePayload._id = users.length + 1
     users.push(messagePayload)
     res.send(users)
 }
 
 const updateUser = (req, res, next ) => {
+    let id = parseInt(req.params.id)
     let messagePayload = req.body
-    let user = users.find((user) => user.id == id)
+    let user = users.find((user) => user.id === id)
     Object.keys(messagePayload).map((key) => {
-        user[key] = payload[key];
+        user[key] = messagePayload[key];
       });
       messagePayload.id = users[id].id - 1
       res.send(users)
 }
 
 const removeUser = (req, res, next) => {
-    let id = parseInt(res.params.id)
-    let user = users.find((user) => user.id == id)
-    users.splice(user, 1)
+    let id = parseInt(req.params.id)
+    let user = users.find((user) => user.id === req.params.id)
+    users.splice(user,1)
+    res.send(users)
 }
 
 module.exports = {getUsers, getUsersById, create, updateUser, removeUser}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser")
 const port = process.env.PORT || 4000
 const users = require("./routes/users")
 
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('default route'))
 app.use(users)
 app.listen(port, () => {
