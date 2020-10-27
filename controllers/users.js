b3e048e9de83ebcf9ed58566ccb0ec748ba0886f
Matt Coston <mattcoston7@gmail.com>
2020-10-27 17:54:16 -0500

    Changed white space

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,19 @@
 const users = require("../data/index")
 
-
 const getUsers = (req, res, next) => {
     res.send(users)
 }
-
 const getUsersById = (req, res, next ) => {
     let id = parseInt(req.params.id)
     let user = users.find((user) => user.id === id)
     res.send(user)
 }
-
 const create = (req, res, next ) => {
     let messagePayload = req.body
     messagePayload._id = users.length + 1
     users.push(messagePayload)
     res.send(users)
 }
-
 const updateUser = (req, res, next ) => {
     let id = parseInt(req.params.id)
     let messagePayload = req.body
@@ -28,12 +24,10 @@ const updateUser = (req, res, next ) => {
       messagePayload.id = users[id].id - 1
       res.send(users)
 }
-
 const removeUser = (req, res, next) => {
     let id = parseInt(req.params.id)
     let user = users.find((user) => user.id === req.params.id)
     users.splice(user,1)
     res.send(users)
 }
-
 module.exports = {getUsers, getUsersById, create, updateUser, removeUser}
